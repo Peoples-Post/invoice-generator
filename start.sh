0dc269f138aef1bcabf6a8a2d8dadfacdd532a82
marcm <marc.morandi@peoplespost.fr>
2026-03-18 14:20:34 +0100

    fix test script 2

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec gunicorn app:app --bind 0.0.0.0:$PORT --workers 1 --timeout 120
+echo "PORT value: $PORT"
+echo "All env vars:"
+env
+exec gunicorn app:app --bind 0.0.0.0:${PORT:-5001} --workers 1 --timeout 120
